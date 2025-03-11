a9708e173d1155762d0b079e4939f4827daa20fd
dikaprio1 <159988070+dikaprio1@users.noreply.github.com>
2025-03-11 22:04:41 +0900

    03/11 22:03
    feat: 주문 동작 완성
    
    주문 받고 결제확인 부분까지 완성
    좀 더 고민해보고 바꿀예정인 부분은
    따로 메모장에 정리해둠

--- src/Kiosk.java
+++ src/Kiosk.java
@@ -23,24 +23,59 @@ public class Kiosk {
             System.out.println("0. 종료");
             System.out.println("원하는 메뉴 번호 입력 :");
             int input = 0;
+            int input2 = 0;
             try {
                 input = sc.nextInt();
                 if(input == 0){
                     System.exit(0);
-                }else if(input > menu.getMenuCategory().size()){
+                }else if(input > menu.getMenuCategory().size() && menu.getCartMenuItems().isEmpty()){
+                    System.out.println("지정되어있는 숫자를 입력해주세요");
+                    continue;
+                }else if(input > menu.getMenuCategory().size()+2 && !menu.getCartMenuItems().isEmpty()){
                     System.out.println("지정되어있는 숫자를 입력해주세요");
                     continue;
                 }else if(menu.getMenuCategory().isEmpty()){
                     System.out.println("카테고리가 없습니다.");
+                    break;
                 }
             } catch (InputMismatchException e) {
                 System.out.println("지정되어있는 숫자를 입력해주세요");
                 continue;
             }
+            if(input == menu.getMenuCategory().size()+1){
                 while(true){
+                    System.out.println("아래와 같이 주문하시겠습니까?");
+                    System.out.println("[Orders]");
+                    menu.printShoppingCartItems();
+                    System.out.println("[Total]");
+                    System.out.println(menu.shoppingCartItemsPrice()+"$");
+                    System.out.println("1. 주문       2. 취소(처음화면으로)");
+                    try {
+                        input2 = sc.nextInt();
+                    } catch (InputMismatchException e) {
+                        System.out.println("지정되어있는 숫자를 입력해주세요");
+                        continue;
+                    }
+                    if(input2 == 1){
+                        System.out.println("주문이 완료되었습니다 금액은 "+menu.shoppingCartItemsPrice()+"$ 입니다.");
+                        menu.getCartMenuItems().clear();
+                        break firstroop;
+                    }else if(input2 == 2){
+                        continue firstroop;
+                    }else{
+                        System.out.println("지정되어있는 숫자를 입력해주세요");
+                    }
+
+                }
+            } else if (input == menu.getMenuCategory().size()+2) {
+                menu.getCartMenuItems().clear();
+                continue;
+            }
+            while(true){
                     menu.getTmpMenuItems().clear();
                     menu.printAllMenuItem(input);
-                    int input2 = 0;
+                    MenuItem m = null;
+                    input2 = 0;
                     try {
                          input2 = sc.nextInt();
                     } catch (InputMismatchException e) {
@@ -51,13 +86,32 @@ public class Kiosk {
                         continue firstroop;
                     }
                     try{
-                        MenuItem m = menu.printMenuItem(input2);
+                        m = menu.printMenuItem(input2);
                         System.out.println("선택된 음식 : "+m);
                     } catch(IndexOutOfBoundsException e){
                         System.out.println("지정되어있는 숫자를 입력해주세요");
                         continue;
                     }
-                    continue firstroop;
+                   while(true){
+                       System.out.println("위 메뉴를 장바구니에 추가하시겠습니까?");
+                       System.out.println("1. 추가       2. 취소");
+                       input2 = 0;
+                       try {
+                           input2 = sc.nextInt();
+                       } catch (InputMismatchException e) {
+                           System.out.println("지정되어있는 숫자를 입력해주세요");
+                           continue;
+                       }
+                       if(input2 == 1){
+                           menu.addCartMenuItems(m);
+                           System.out.println(m.getName()+"가(이) 장바구니에 추가되었습니다.");
+                           continue firstroop;
+                       }else if(input2 == 2){
+                           continue firstroop;
+                       }else{
+                           System.out.println("지정되어있는 숫자를 입력해주세요");
+                       }
+                   }
                 }
         }
     }
--- src/Menu.java
+++ src/Menu.java
@@ -5,11 +5,13 @@ public class Menu {
 
     private List<Category> menuCategory;
     private List<MenuItem> menuItems;
+    private List<MenuItem> cartMenuItems;
     private List<MenuItem> tmpMenuItems;
 
     public Menu(){
         menuCategory = new ArrayList<>();
         menuItems = new ArrayList<>();
+        cartMenuItems = new ArrayList<>();
         tmpMenuItems = new ArrayList<>();
     }
 
@@ -17,20 +19,28 @@ public class Menu {
     public  void addMenuCategory(Category category){
         menuCategory.add(category);
     }
-
     // 소분류 추가(추후 관리자에서 사용예정) ------------------------------------------------------------
     public  void addMenuItem(MenuItem menuItem){
         menuItems.add(menuItem);
     }
+    // 장바구니 리스트 소분류 추가 ------------------------------------------------------------
+    public  void addCartMenuItems(MenuItem MenuItems){
+        cartMenuItems.add(MenuItems);
+    }
     // 다른 리스트 소분류 추가 ------------------------------------------------------------
-    public  void addTmpMenuItems(MenuItem tmpmenuItems){
-        tmpMenuItems.add(tmpmenuItems);
+    public  void addTmpMenuItems(MenuItem MenuItems){
+        tmpMenuItems.add(MenuItems);
     }
 
     //모든 대분류 출력 ------------------------------------------------------------
     public void printAllMenuCategory(){
         for(Category c : menuCategory){
             System.out.println(c);
+        }
+        if(!cartMenuItems.isEmpty()) {
+            System.out.println("[ORDER MENU]");
+            System.out.println(menuCategory.size() + 1 + ". Orders");
+            System.out.println(menuCategory.size() + 2 + ". Cancel(장바구니 초기화)");
         }
     }
     //선택된 대분류의 모든 소분류 출력 ------------------------------------------------------------
@@ -52,30 +62,49 @@ public class Menu {
     public MenuItem printMenuItem(int input){
            return tmpMenuItems.get(input-1);
     }
-
+    public void printShoppingCartItems(){
+        for(MenuItem i : cartMenuItems){
+            System.out.println(i);
+        }
+    }
+    public Double shoppingCartItemsPrice(){
+        Double sum = 0.0;
+        for(MenuItem i : cartMenuItems){
+            sum += i.getPrice();
+        }
+        return sum;
+    }
+    
+    
+    
+    
+    
+    
     //게터 세터 ------------------------------------------------------------
-
     public List<Category> getMenuCategory() {
         return menuCategory;
     }
-
     public void setMenuCategory(List<Category> menuCategory) {
         this.menuCategory = menuCategory;
     }
-
     public List<MenuItem> getMenuItems() {
         return menuItems;
     }
-
     public void setMenuItems(List<MenuItem> menuItems) {
         this.menuItems = menuItems;
     }
-
     public List<MenuItem> getTmpMenuItems() {
         return tmpMenuItems;
     }
-
     public void setTmpMenuItems(List<MenuItem> tmpMenuItems) {
         this.tmpMenuItems = tmpMenuItems;
+    }
+    
+    public List<MenuItem> getCartMenuItems() {
+        return cartMenuItems;
+    }
+
+    public void setCartMenuItems(List<MenuItem> cartMenuItems) {
+        this.cartMenuItems = cartMenuItems;
     }
 }
